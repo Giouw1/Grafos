b8cb3ae36039e02f9984ed99f5e7c26cccd1fc9f
Enzo Sampaio <144063703+Voltyz98@users.noreply.github.com>
2024-10-23 16:07:46 -0300

    heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>  // Inclui todas as bibliotecas padrão do C++
 
 using namespace std;
+
 class Heap {
 private:
     vector<int> heap;  // Armazena os elementos do heap
     unordered_map<int, int> indiceMapa;  // Mapeia chaves para seus índices no heap
-    int qtd;  // Quantidade de elementos no heap
+    int qtd = 0;  // Quantidade de elementos no heap
     // troca os elementos no heap e atualiza o indiceMapa, para praticidade
     void trocar(int i, int j) {
         swap(heap[i], heap[j]);
@@ -41,24 +42,28 @@ private:
         heap.resize(heap.size() * 2);
     }
 public:
-    Heap() : qtd(0) {
-        heap.resize(2000);  // Começa com capacidade 2000 (index 0 não é usado)
+    Heap(int n) : qtd(0) {
+        heap.resize(n);  // Começa com capacidade 2000 (index 0 não é usado)
     }
     // Insere um novo elemento no heap
-    void insert(int key) {
+    void insert(int chave, int index) {
         if (qtd + 1 == heap.size()) {
             redimensionar();
         }
         qtd++;
-        heap[qtd] = key;
-        indiceMapa[key] = qtd;
+        heap[qtd] = chave;
+        indiceMapa[index] = chave;
         subir(qtd);
     }
     // Remove o elemento do topo (mínimo, pois é MinHeap)
+    bool estaVazia(){
+        if(qtd == 0)return true;
+        return false;
+    }
     int extrairMin() {
         if (qtd == 0) throw runtime_error("Heap esta vazia");
         int minVal = heap[1];
-        trocar(1, qtd);
+        if(qtd != 1) trocar(1, qtd);
         qtd--;
         descer(1);
         indiceMapa.erase(minVal);
